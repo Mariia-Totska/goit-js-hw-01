2ebb0d076fb0ae25a2bc1229425a13c8862682d4
Mariia Totska <mtockva@gmail.com>
2024-09-15 18:57:25 +0300

    corr task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 function getElementWidth(content, padding, border) {
-    return `${
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border)
-  }px`;
+
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding) * 2; 
+  const borderWidth = parseFloat(border) * 2; 
+
+  return contentWidth + paddingWidth + borderWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
